64e985e8893e0f27ad968c63ee3aa7e059b6e3cc
alex <alex@tekartik.com>
2020-05-01 00:15:37 +0200

    test: dart 2.9 formatting

--- tool/travis.dart
+++ tool/travis.dart
@@ -1,4 +1,5 @@
 import 'package:process_run/shell.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 Future main() async {
   var shell = Shell();
@@ -6,9 +7,12 @@ Future main() async {
   await shell.run('''
 # Analyze code
 dartanalyzer --fatal-warnings --fatal-infos .
-dartfmt -n --set-exit-if-changed .
-
 # Run tests
 pub run test -p vm
 ''');
+
+  // Formatting change int 2.9 with hashbang first line
+  if (dartVersion >= Version(2, 9, 0, pre: '0')) {
+    await shell.run('dartfmt -n --set-exit-if-changed .');
+  }
 }
